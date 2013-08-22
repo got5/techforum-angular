7213a08052a5677683dd5855544758001d3bcfe2
ffacon <francois.facon@gmail.com>
2013-08-22 15:42:09 +0200

    New configuration variable for latest nodejs cartridge

--- openshift-server/server.js
+++ openshift-server/server.js
@@ -27,8 +27,8 @@ var TechForumApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_INTERNAL_IP;
-        self.port      = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
